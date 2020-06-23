d26e3aca848126edef8d7f7de500ceb8dd907cf0
Eren Guldas <erenguldas@gmail.com>
2020-06-23 07:28:54 +0300

    key

--- src/components/App.js
+++ src/components/App.js
@@ -18,10 +18,10 @@ class App extends React.Component {
       'https://www.googleapis.com/youtube/v3/search',
       {
         params: {
+          q: inputText,
           part: 'snippet',
           maxResults: 10,
           key: process.env.REACT_APP_API_KEY,
-          q: inputText,
         },
       }
     )
